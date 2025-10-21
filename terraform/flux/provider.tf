cf664b9f7c89d1d1a525d63e3fc25a741d0439aa
Murtaza Udaipurwala <murtaza@murtazau.xyz>
2025-10-21 12:22:19 +0530

    revert: set branch to main

--- terraform/flux/provider.tf
+++ terraform/flux/provider.tf
@@ -52,6 +52,6 @@ provider "flux" {
     }
     author_email = "bot@fluxcd.io"
     author_name  = "Flux Bot"
-    branch       = "refactor/nixos-and-gh-action-pipeline"
+    branch       = "main"
   }
 }
